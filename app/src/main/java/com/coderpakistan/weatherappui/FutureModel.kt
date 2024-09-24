d537d4ada929cc2e6a49af35067566035d0bb44e
Amir Hamza <amirhamza@Amirs-MacBook-Pro.local>
2024-09-24 21:02:53 +0100

    Initial commit

---
+++ app/src/main/java/com/coderpakistan/weatherappui/FutureModel.kt
@@ -0,0 +1,9 @@
+package com.coderpakistan.weatherappui
+
+data class FutureModel(
+    val day: String,
+    val picPath: String,
+    val status: String,
+    val lowTemp: Int,
+    val highTemp: Int
+)
